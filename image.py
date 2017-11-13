7dfa468803133665802ab0c59dc0017ef216c59a
youngyi <727672609@qq.com>
2017-11-13 21:10:24 +0800

    Initial commit

---
+++ image.py
@@ -0,0 +1,20 @@
+from skimage.io import imread
+from skimage.io import imsave
+from skimage.transform import resize
+from matplotlib import pyplot as plt
+import os
+
+train = open("val.txt", "r+")
+
+def process(file_):
+	im = imread(file_)
+	image = resize(im, (224, 224))
+	output = file_.split("/")
+	fname = os.path.join("val", output[2], output[3])
+	imsave(fname,image)
+
+#process("yas.jpg")
+
+for line in train:
+	values = line.split()[0]
+	process(values)
