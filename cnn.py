c332afda7be127c4ae1fac6c1d12ce6f3d55a5aa
YangYi <yyangcp@ust.hk>
2017-11-16 14:55:18 +0800

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -137,6 +137,6 @@ optimizer_ft = optim.Adam(model_ft.parameters())
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-					   num_epochs=25)
+					   num_epochs=40)
 
 ######################################################################
