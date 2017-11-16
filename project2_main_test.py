22cf441b7dcbd7b9a94317d7e0d7ff83a3729783
YangYi <yyangcp@ust.hk>
2017-11-16 14:39:56 +0800

    Fine-Tuning combine with the cnn model
    base on transfer learning and the cnn 
    choose this method to make the classify be easy and faster

---
+++ project2_main_test.py
@@ -0,0 +1,61 @@
+from __future__ import print_function, division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+
+
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+        'test': transforms.Compose([
+                transforms.Scale(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+}
+
+data_dir = 'flowers'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                                                                  data_transforms[x])
+                                  for x in ['test']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=64,
+                                                                                         shuffle=False, num_workers=4)
+                                  for x in ['test']}
+use_gpu = torch.cuda.is_available()
+
+# Get a batch of training data
+inputs = next(iter(dataloaders['test.txt']))
+
+# Make a grid from batch
+def train_model(model, file):
+    since = time.time()
+    model.train(False)
+    for data in dataloaders['test']:
+                inputs,labels = data
+                if use_gpu:
+                   inputs= Variable(inputs.cuda())
+                else:
+                   inputs = Variable(inputs)
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                print(preds)
+                file.write(str(preds)+'\n')
+#####################################################
+# Test model
+def restore_cnn():
+        w_file = open('project2_20459219', 'w')
+        cnn2 = torch.load('cnn.pkl')
+        if use_gpu:
+            cnn2 = cnn2.cuda()
+        train_model(cnn2, w_file)
+        w_file.close()
+restore_cnn()
